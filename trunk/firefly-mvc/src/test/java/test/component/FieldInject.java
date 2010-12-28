2af9eadee7c9083bd20907e149589f095f73917e
baldulista@gmail.com <baldulista@gmail.com@4c6c975e-b6eb-aeeb-d668-919e69cb0141>
2010-12-28 16:50:40 +0000

    up

--- trunk/firefly-mvc/src/test/java/test/component/FieldInject.java
+++ trunk/firefly-mvc/src/test/java/test/component/FieldInject.java
@@ -2,4 +2,6 @@ package test.component;
 
 public interface FieldInject {
 	int add(int x, int y);
+
+	int add2(int x, int y);
 }
--- trunk/firefly-mvc/src/test/java/test/component/impl/FieldInjectImpl.java
+++ trunk/firefly-mvc/src/test/java/test/component/impl/FieldInjectImpl.java
@@ -10,10 +10,17 @@ public class FieldInjectImpl implements FieldInject {
 
 	@Inject
 	private AddService addService;
+	@Inject("addService")
+	private AddService addService2;
 
 	@Override
 	public int add(int x, int y) {
 		return addService.add(x, y);
+	}
+
+	@Override
+	public int add2(int x, int y) {
+		return addService2.add(x, y);
 	}
 
 }
