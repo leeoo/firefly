8dd30da22b8c4a7666384c48c0f7711a30d6fd57
baldulista@gmail.com <baldulista@gmail.com@4c6c975e-b6eb-aeeb-d668-919e69cb0141>
2010-12-28 15:58:08 +0000

    up

--- trunk/firefly-mvc/src/main/java/com/test/sample/controller/Test1Controller.java
+++ trunk/firefly-mvc/src/main/java/com/test/sample/controller/Test1Controller.java
@@ -4,11 +4,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.firefly.annotation.Controller;
+import com.firefly.annotation.Inject;
 import com.firefly.annotation.RequestMapping;
 import com.firefly.mvc.web.View;
+import com.test.sample.service.Test2Service;
 
 @Controller
 public class Test1Controller {
+	private Test2Service test2Service;
+
+	@SuppressWarnings("unused")
+	@Inject
+	private void init(Test2Service test2Service) {
+		this.test2Service = test2Service;
+	}
 
 	@RequestMapping(value = "/hello")
 	public String index(HttpServletRequest request) {
@@ -22,6 +31,6 @@ public class Test1Controller {
 	public String hello1(HttpServletResponse response,
 			HttpServletRequest request) {
 
-		return "测试一下hello1";
+		return "测试一下 3 + 3 =" + test2Service.add(3, 3);
 	}
 }
