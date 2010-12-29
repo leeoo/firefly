c0e2459ad6b2937f16e4331383ade58a32016a3c
baldulista@gmail.com <baldulista@gmail.com@4c6c975e-b6eb-aeeb-d668-919e69cb0141>
2010-12-29 03:59:26 +0000

    up

--- firefly-mvc/src/main/java/com/test/sample/controller/Hello2Controller.java
+++ firefly-mvc/src/main/java/com/test/sample/controller/Hello2Controller.java
@@ -9,22 +9,23 @@ import org.slf4j.LoggerFactory;
 import com.firefly.annotation.Component;
 import com.firefly.annotation.Inject;
 import com.firefly.annotation.RequestMapping;
-import com.test.sample.service.Test2Service;
+import com.test.sample.service.AddService;
 
 @Component("test2Hello")
-public class Test2Controller {
-	private static Logger log = LoggerFactory.getLogger(Test2Controller.class);
+public class Hello2Controller {
+	private static Logger log = LoggerFactory.getLogger(Hello2Controller.class);
 	@Inject
-	private Test2Service test2Service;
+	private AddService addService;
 
 	@RequestMapping(value = "/hello2")
 	public String index(HttpServletResponse response, HttpServletRequest request) {
-//		WebContext webContext = WebContextHolder.getWebContext();
-//		Test2Controller t = (Test2Controller) webContext.getBean("test2Hello");
-//		t.test();
-//		t = (Test2Controller) webContext.getBean(Test2Controller.class
-//				.getName());
-//		t.test();
+		// WebContext webContext = WebContextHolder.getWebContext();
+		// Test2Controller t = (Test2Controller)
+		// webContext.getBean("test2Hello");
+		// t.test();
+		// t = (Test2Controller) webContext.getBean(Test2Controller.class
+		// .getName());
+		// t.test();
 
 		request.setAttribute("hello", "test2 hello firefly! ");
 
@@ -35,8 +36,11 @@ public class Test2Controller {
 	public String hello3(HttpServletRequest request,
 			HttpServletResponse response) {
 		try {
-			log.info("hello3 req encoding [{}]", request.getCharacterEncoding());
-			log.info("hello3 res encoding [{}]", response.getCharacterEncoding());
+			log
+					.info("hello3 req encoding [{}]", request
+							.getCharacterEncoding());
+			log.info("hello3 res encoding [{}]", response
+					.getCharacterEncoding());
 			response.setHeader("Content-Type", "text/plain; charset=utf-8");
 			PrintWriter writer = response.getWriter();
 			writer.print("测试hello3");
@@ -51,7 +55,7 @@ public class Test2Controller {
 	@RequestMapping(value = "/hello4")
 	public String hello4(HttpServletRequest request,
 			HttpServletResponse response) {
-		int i = test2Service.add(99, 100);
+		int i = addService.add(99, 100);
 		request.setAttribute("hello", "test2 hello inject! " + i);
 		return "/index.jsp";
 	}
