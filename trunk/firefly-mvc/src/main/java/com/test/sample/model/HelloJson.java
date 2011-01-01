b7db30a1e4fc46d5c583c451f6e94e4bad9ff66b
baldulista@gmail.com <baldulista@gmail.com@4c6c975e-b6eb-aeeb-d668-919e69cb0141>
2011-01-01 22:47:14 +0000

    up

---
+++ trunk/firefly-mvc/src/main/java/com/test/sample/model/HelloJson.java
@@ -0,0 +1,22 @@
+package com.test.sample.model;
+
+public class HelloJson {
+	private String text;
+	private Integer id;
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+}
