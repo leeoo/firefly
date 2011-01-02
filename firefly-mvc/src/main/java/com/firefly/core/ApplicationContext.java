0a05f1d0491ea488ab21d1920666698e8ba6fed1
baldulista@gmail.com <baldulista@gmail.com@4c6c975e-b6eb-aeeb-d668-919e69cb0141>
2011-01-02 08:08:06 +0000

    delete

--- firefly-mvc/src/main/java/com/firefly/core/ApplicationContext.java
+++
@@ -1,5 +0,0 @@
-package com.firefly.core;
-
-public interface ApplicationContext {
-	public Object getBean(String id);
-}
--- firefly-mvc/src/main/java/com/firefly/mvc/web/View.java
+++
@@ -1,8 +0,0 @@
-package com.firefly.mvc.web;
-
-public interface View {
-	String JSP = "jsp";
-	String JSON = "json";
-	String TEXT = "text";
-	String REDIRECT = "redirect";
-}
