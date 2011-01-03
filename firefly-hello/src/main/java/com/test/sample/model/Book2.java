d7e31982ffc18943ff6a1ccb414224b85264e81f
baldulista@gmail.com <baldulista@gmail.com@4c6c975e-b6eb-aeeb-d668-919e69cb0141>
2011-01-03 23:30:13 +0000

    up

---
+++ firefly-hello/src/main/java/com/test/sample/model/Book2.java
@@ -0,0 +1,14 @@
+package com.test.sample.model;
+
+public class Book2 {
+	private Float price;
+
+	public Float getPrice() {
+		return price;
+	}
+
+	public void setPrice(Float price) {
+		this.price = price;
+	}
+
+}
