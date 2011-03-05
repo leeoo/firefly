38f22b61be59942de01a16005281b41053580b29
baldulista@gmail.com <baldulista@gmail.com@4c6c975e-b6eb-aeeb-d668-919e69cb0141>
2011-03-05 07:32:57 +0000

    up

--- firefly-core/src/main/java/com/firefly/core/support/BeanReader.java
+++ firefly-core/src/main/java/com/firefly/core/support/BeanReader.java
@@ -2,8 +2,6 @@ package com.firefly.core.support;
 
 import java.util.List;
 
-import com.firefly.utils.Pair;
-
 public interface BeanReader {
-	List<Pair<Class<?>, Object>> getClasses();
+	List<BeanDefinition> loadBeanDefinitions();
 }
