49062ef59133a786f03ee186e0de764081aef2ab
baldulista@gmail.com <baldulista@gmail.com@4c6c975e-b6eb-aeeb-d668-919e69cb0141>
2011-03-14 18:15:43 +0000

    增加测试用例

--- trunk/firefly-core/src/test/java/test/ioc/TestAnnotationIoc.java
+++ trunk/firefly-core/src/test/java/test/ioc/TestAnnotationIoc.java
@@ -1,30 +1,19 @@
 package test.ioc;
 
-import static org.hamcrest.Matchers.greaterThan;
-import static org.hamcrest.Matchers.is;
-
-import java.util.List;
-
+import static org.hamcrest.Matchers.*;
 import org.junit.Assert;
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import test.component.AddService;
 import test.component.FieldInject;
 import test.component.MethodInject;
 import test.component2.MethodInject2;
-import test.component3.Person;
-import test.component3.PersonService;
-
 import com.firefly.core.AnnotationApplicationContext;
 import com.firefly.core.ApplicationContext;
-import com.firefly.core.XmlApplicationContext;
 
-public class TestIoc {
-	private static Logger log = LoggerFactory.getLogger(TestIoc.class);
+
+public class TestAnnotationIoc {
+//	private static Logger log = LoggerFactory.getLogger(TestAnnotationIoc.class);
 	public static ApplicationContext applicationContext = new AnnotationApplicationContext();
-	public static ApplicationContext xmlApplicationContext = new XmlApplicationContext();
 	
 	@Test
 	public void testFieldInject() {
@@ -58,23 +47,4 @@ public class TestIoc {
 		Assert.assertThat(t.getI(), greaterThan(0));
 	}
 	
-	@Test
-	public void testXmlInject(){
-		Person person = xmlApplicationContext.getBean("person");
-		Assert.assertThat(person.getName(), is("Jack"));
-		PersonService personService = xmlApplicationContext.getBean("personService");
-		List<Object> l = personService.getTestList();
-		Assert.assertThat(l.size(), greaterThan(0));
-		int i = 0;
-		for(Object p : l){
-			if(p instanceof Person){
-				person = (Person)p;
-				i++;
-				log.info(person.getName());
-			}else{
-				log.info(String.valueOf(p));
-			}
-		}
-		Assert.assertThat(i, greaterThan(1));
-	}
 }
---
+++ trunk/firefly-core/src/test/java/test/ioc/TestXmlIoc.java
@@ -0,0 +1,58 @@
+package test.ioc;
+
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.is;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import test.component3.CollectionService;
+import test.component3.Person;
+import test.component3.PersonService;
+
+import com.firefly.core.ApplicationContext;
+import com.firefly.core.XmlApplicationContext;
+
+public class TestXmlIoc {
+	private static Logger log = LoggerFactory.getLogger(TestXmlIoc.class);
+	public static ApplicationContext xmlApplicationContext = new XmlApplicationContext();
+
+	@Test
+	public void testXmlInject() {
+		Person person = xmlApplicationContext.getBean("person");
+		Assert.assertThat(person.getName(), is("Jack"));
+		PersonService personService = xmlApplicationContext
+				.getBean("personService");
+		List<Object> l = personService.getTestList();
+		Assert.assertThat(l.size(), greaterThan(0));
+		int i = 0;
+		for (Object p : l) {
+			if (p instanceof Person) {
+				person = (Person) p;
+				i++;
+				log.info(person.getName());
+			} else {
+				log.info(p.toString());
+			}
+		}
+		Assert.assertThat(i, greaterThan(1));
+	}
+
+	@Test
+	public void testXmlLinkedListInject() {
+		// 注入的不仅仅是List
+		CollectionService collectionService = xmlApplicationContext
+				.getBean("collectionService");
+		List<Object> list = collectionService.getList();
+		Assert.assertThat(list.size(), greaterThan(0));
+
+		// list的值也是list
+		collectionService = xmlApplicationContext.getBean("collectionService2");
+		list = collectionService.getList();
+		Assert.assertThat(list.size(), greaterThan(0));
+	}
+}
