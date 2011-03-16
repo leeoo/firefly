6346b1e18ec3d2a9b12bf660450cdffb4f807f23
baldulista@gmail.com <baldulista@gmail.com@4c6c975e-b6eb-aeeb-d668-919e69cb0141>
2011-03-16 08:57:05 +0000

    up

--- trunk/firefly-core/src/main/java/com/firefly/utils/Cast.java
+++ trunk/firefly-core/src/main/java/com/firefly/utils/Cast.java
@@ -5,22 +5,25 @@ abstract public class Cast {
 	@SuppressWarnings("unchecked")
 	public static <T> T convert(String value, Class<T> c) {
 		Object ret = null;
-		if (c.equals(Integer.class)) {
+		if (c.equals(int.class) || c.equals(Integer.class)) {
 			ret = Integer.parseInt(value);
-		} else if (c.equals(Long.class)) {
+		} else if (c.equals(long.class) || c.equals(Long.class)) {
 			ret = Long.parseLong(value);
-		} else if (c.equals(Double.class)) {
+		} else if (c.equals(double.class) || c.equals(Double.class)) {
 			ret = Double.parseDouble(value);
-		} else if (c.equals(Float.class)) {
+		} else if (c.equals(float.class) || c.equals(Float.class)) {
 			ret = Float.parseFloat(value);
-		} else if (c.equals(Boolean.class)) {
+		} else if (c.equals(boolean.class) || c.equals(Boolean.class)) {
 			ret = Boolean.parseBoolean(value);
-		} else if (c.equals(Short.class)) {
+		} else if (c.equals(short.class) || c.equals(Short.class)) {
 			ret = Short.parseShort(value);
-		} else if (c.equals(Byte.class)) {
+		} else if (c.equals(byte.class) || c.equals(Byte.class)) {
 			ret = Byte.parseByte(value);
-		} else if (c.equals(String.class)) {
-			ret = value;
+		} else {
+			if (VerifyUtils.isNumeric(value))
+				ret = new Integer(value);
+			else
+				ret = value;
 		}
 		return (T) ret;
 	}
@@ -28,33 +31,21 @@ abstract public class Cast {
 	@SuppressWarnings("unchecked")
 	public static <T> T convert(String value, String argsType) {
 		Object ret = null;
-		if ("byte".equals(argsType))
-			ret = Byte.parseByte(value);
-		else if ("short".equals(argsType))
-			ret = Short.parseShort(value);
-		else if ("int".equals(argsType))
-			ret = Integer.parseInt(value);
-		else if ("long".equals(argsType))
-			ret = Long.parseLong(value);
-		else if ("float".equals(argsType))
-			ret = Float.parseFloat(value);
-		else if ("double".equals(argsType))
-			ret = Double.parseDouble(value);
-		else if ("boolean".equals(argsType))
-			ret = Boolean.parseBoolean(value);
-		else if ("java.lang.Byte".equals(argsType))
+		if ("byte".equals(argsType) || "java.lang.Byte".equals(argsType))
 			ret = new Byte(value);
-		else if ("java.lang.Short".equals(argsType))
+		else if ("short".equals(argsType) || "java.lang.Short".equals(argsType))
 			ret = new Short(value);
-		else if ("java.lang.Integer".equals(argsType))
+		else if ("int".equals(argsType) || "java.lang.Integer".equals(argsType))
 			ret = new Integer(value);
-		else if ("java.lang.Long".equals(argsType))
+		else if ("long".equals(argsType) || "java.lang.Long".equals(argsType))
 			ret = new Long(value);
-		else if ("java.lang.Float".equals(argsType))
+		else if ("float".equals(argsType) || "java.lang.Float".equals(argsType))
 			ret = new Float(value);
-		else if ("java.lang.Double".equals(argsType))
+		else if ("double".equals(argsType)
+				|| "java.lang.Double".equals(argsType))
 			ret = new Double(value);
-		else if ("java.lang.Boolean".equals(argsType))
+		else if ("boolean".equals(argsType)
+				|| "java.lang.Boolean".equals(argsType))
 			ret = new Boolean(value);
 		else {
 			if (VerifyUtils.isNumeric(value))
