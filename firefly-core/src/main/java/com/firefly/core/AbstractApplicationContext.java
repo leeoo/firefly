145faf53fd60dc1ceeed6596c34fd8ae7fa13298
baldulista@gmail.com <baldulista@gmail.com@4c6c975e-b6eb-aeeb-d668-919e69cb0141>
2011-03-20 16:36:26 +0000

    加入了混合注入的冲突检测

--- firefly-core/src/main/java/com/firefly/core/AbstractApplicationContext.java
+++ firefly-core/src/main/java/com/firefly/core/AbstractApplicationContext.java
@@ -4,23 +4,31 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.firefly.core.support.BeanDefinition;
+import com.firefly.core.support.exception.BeanDefinitionParsingException;
 import com.firefly.utils.VerifyUtils;
 
 abstract public class AbstractApplicationContext implements ApplicationContext {
 
+	private static Logger log = LoggerFactory
+			.getLogger(AbstractApplicationContext.class);
 	protected Map<String, Object> map = new HashMap<String, Object>();
 	protected List<BeanDefinition> beanDefinitions;
-	
+
 	public AbstractApplicationContext() {
 		this(null);
 	}
-	
+
 	public AbstractApplicationContext(String file) {
 		beanDefinitions = getBeanDefinitions(file);
+		check(); //冲突检测
 		addObjectToContext();
 	}
-	
+
 	private void addObjectToContext() {
 		for (BeanDefinition beanDefinition : beanDefinitions) {
 			inject(beanDefinition);
@@ -38,7 +46,50 @@ abstract public class AbstractApplicationContext implements ApplicationContext {
 	public <T> T getBean(String id) {
 		return (T) map.get(id);
 	}
-	
+
+	protected void check() {
+		// TODO 第3个还需要实现
+		// 1.id相同的抛异常
+		// 2.className或者interfaceName相同，但其中一个没有定义id，抛异常
+		// 3.className或者interfaceName相同，且都定义的id，需要保存备忘，按类型或者接口自动注入的时候抛异常
+		for (BeanDefinition b1 : beanDefinitions) {
+			for (BeanDefinition b2 : beanDefinitions) {
+				// 同一个beanDefinition不需要比较
+				if(b1 == b2)
+					continue;
+				
+				if (VerifyUtils.isNotEmpty(b1.getId())
+						&& VerifyUtils.isNotEmpty(b2.getId())
+						&& b1.getId().equals(b2.getId())) {
+					error("bean " + b1.getClassName() + " and bean "
+							+ b2.getClassName() + " have duplicate id ");
+				}
+
+				if (b1.getClassName().equals(b2.getClassName())) {
+					if (VerifyUtils.isEmpty(b1.getId())
+							|| VerifyUtils.isEmpty(b2.getId())) {
+						error("class " + b1.getClassName()
+								+ " redundant definition");
+					}
+				}
+
+				Set<String> i1 = b1.getInterfaceNames();
+				Set<String> i2 = b2.getInterfaceNames();
+				for (String iname1 : i1) {
+					for (String iname2 : i2) {
+						if (iname1.equals(iname2)) {
+							if (VerifyUtils.isEmpty(b1.getId())
+									|| VerifyUtils.isEmpty(b2.getId())) {
+								error("class " + b1.getClassName()
+										+ " redundant definition");
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
 	protected void addObjectToContext(BeanDefinition beanDefinition) {
 		// 增加声明的组件到 ApplicationContext
 		Object object = beanDefinition.getObject();
@@ -56,7 +107,7 @@ abstract public class AbstractApplicationContext implements ApplicationContext {
 			map.put(k, object);
 		}
 	}
-	
+
 	protected BeanDefinition findBeanDefinition(String key) {
 		for (BeanDefinition beanDefinition : beanDefinitions) {
 			if (key.equals(beanDefinition.getId())) {
@@ -72,9 +123,20 @@ abstract public class AbstractApplicationContext implements ApplicationContext {
 		}
 		return null;
 	}
-	
+
+	/**
+	 * 处理异常
+	 * 
+	 * @param msg
+	 *            异常信息
+	 */
+	protected void error(String msg) {
+		log.error(msg);
+		throw new BeanDefinitionParsingException(msg);
+	}
+
 	abstract protected List<BeanDefinition> getBeanDefinitions(String file);
-	
+
 	abstract protected Object inject(BeanDefinition beanDef);
 
 }
