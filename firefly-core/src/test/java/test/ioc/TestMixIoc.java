4657be51fa9427cd50db89c7d2dcd57b4605f053
baldulista@gmail.com <baldulista@gmail.com@4c6c975e-b6eb-aeeb-d668-919e69cb0141>
2011-03-21 07:05:49 +0000

    up

--- firefly-core/src/test/java/test/ioc/TestMixIoc.java
+++ firefly-core/src/test/java/test/ioc/TestMixIoc.java
@@ -1,14 +1,18 @@
 package test.ioc;
 
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.is;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import test.mixed.Food;
 import test.mixed.FoodService;
+
 import com.firefly.core.ApplicationContext;
 import com.firefly.core.XmlApplicationContext;
+import com.firefly.core.support.exception.BeanDefinitionParsingException;
 
 public class TestMixIoc {
 	private static Logger log = LoggerFactory.getLogger(TestMixIoc.class);
@@ -24,5 +28,25 @@ public class TestMixIoc {
 		food = foodService.getFood("strawberry");
 		log.debug(food.getName());
 		Assert.assertThat(food.getPrice(), is(10.00));
+	}
+
+	@Test(expected = BeanDefinitionParsingException.class)
+	public void testErrorConfig1() {
+		new XmlApplicationContext("error-config1.xml");
+	}
+
+	@Test(expected = BeanDefinitionParsingException.class)
+	public void testErrorConfig2() {
+		new XmlApplicationContext("error-config2.xml");
+	}
+
+	@Test(expected = BeanDefinitionParsingException.class)
+	public void testErrorConfig3() {
+		new XmlApplicationContext("error-config3.xml");
+	}
+
+	@Test(expected = BeanDefinitionParsingException.class)
+	public void testErrorConfig4() {
+		new XmlApplicationContext("error-config4.xml");
 	}
 }
