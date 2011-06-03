fe70e025249f4757e3d14b2989e7bf2c8b3779e9
baldulista@gmail.com <baldulista@gmail.com@4c6c975e-b6eb-aeeb-d668-919e69cb0141>
2011-06-03 06:09:29 +0000

    up

--- firefly-nettool/src/test/java/test/net/tcp/TestTcpServer.java
+++ firefly-nettool/src/test/java/test/net/tcp/TestTcpServer.java
@@ -5,7 +5,7 @@ import com.firefly.net.tcp.TcpServer;
 public class TestTcpServer {
 
 	public static void main(String[] args) {
-		new TcpServer("10.4.78.19", 9900, new StringLineDecoder(),
+		new TcpServer("localhost", 9900, new StringLineDecoder(),
 				new StringLineEncoder(), new StringLineHandler()).start();
 	}
 }
