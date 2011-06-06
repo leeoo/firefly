af828fb75988b887f55dd4fd1d46b752d6965b12
baldulista@gmail.com <baldulista@gmail.com@4c6c975e-b6eb-aeeb-d668-919e69cb0141>
2011-06-06 05:25:41 +0000

    up

--- firefly-nettool/src/test/java/test/net/tcp/TestTcpClient.java
+++ firefly-nettool/src/test/java/test/net/tcp/TestTcpClient.java
@@ -1,57 +1,57 @@
 package test.net.tcp;
 
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
 import com.firefly.net.Client;
+import com.firefly.net.ClientConnectionPool;
 import com.firefly.net.Handler;
 import com.firefly.net.Session;
 import com.firefly.net.tcp.TcpClient;
 
 public class TestTcpClient {
 	public static void main(String[] args) {
-		final BlockingQueue<Session> queue = new LinkedBlockingQueue<Session>();
-		final BlockingQueue<String> receive = new LinkedBlockingQueue<String>();
-		Client client = new TcpClient(new StringLineDecoder(), new StringLineEncoder(), new Handler() {
+		final ClientConnectionPool clientConnectionPool = new ClientConnectionPool(
+				1024, 1024, 1000);
+		Client client = new TcpClient(new StringLineDecoder(),
+				new StringLineEncoder(), new Handler() {
 
-			@Override
-			public void sessionOpened(Session session) {
-				System.out.println("client session open |" + session.getSessionId());
-				queue.offer(session);
-			}
+					@Override
+					public void sessionOpened(Session session) {
+						System.out.println("client session open |"
+								+ session.getSessionId());
+						clientConnectionPool.putSession(session);
+					}
 
-			@Override
-			public void sessionClosed(Session session) {
-				System.out.println("client session close|" + session.getSessionId());
-			}
+					@Override
+					public void sessionClosed(Session session) {
+						System.out.println("client session close|"
+								+ session.getSessionId());
+					}
 
-			@Override
-			public void messageRecieved(Session session, Object message) {
-				String str = (String) message;
-				receive.offer(str);
-			}
+					@Override
+					public void messageRecieved(Session session, Object message) {
+						String str = (String) message;
+						clientConnectionPool.putReceive(str);
+					}
 
-			@Override
-			public void exceptionCaught(Session session, Throwable t) {
-				System.out.println(t.getMessage() + "|"
-						+ session.getSessionId());
-			}
-		});
+					@Override
+					public void exceptionCaught(Session session, Throwable t) {
+						System.out.println(t.getMessage() + "|"
+								+ session.getSessionId());
+					}
+				});
 		client.connect("192.168.1.102", 9900);
-		try {
-			Session session = queue.take();
-			session.encode("hello client");
-			String ret = receive.take();
-			System.out.println("receive[" + ret + "]");
-			
-			session.encode("test2");
-			ret = receive.take();
-			System.out.println("receive[" + ret + "]");
-			
-			session.encode("quit");
-			ret = receive.take();
-			System.out.println("receive[" + ret + "]");
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+
+		Session session = clientConnectionPool.getSession();
+		session.encode("hello client");
+		String ret = (String) clientConnectionPool.getReceive();
+		System.out.println("receive[" + ret + "]");
+
+		session.encode("test2");
+		ret = (String) clientConnectionPool.getReceive();
+		System.out.println("receive[" + ret + "]");
+
+		session.encode("quit");
+		ret = (String) clientConnectionPool.getReceive();
+		System.out.println("receive[" + ret + "]");
+
 	}
 }
