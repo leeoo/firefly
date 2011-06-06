b0042d8f416ca1c1451e8114861697197e123dfb
baldulista@gmail.com <baldulista@gmail.com@4c6c975e-b6eb-aeeb-d668-919e69cb0141>
2011-06-06 05:25:41 +0000

    up

---
+++ trunk/firefly-nettool/src/main/java/com/firefly/net/ClientConnectionPool.java
@@ -0,0 +1,126 @@
+package com.firefly.net;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ClientConnectionPool {
+	private static Logger log = LoggerFactory
+			.getLogger(ClientConnectionPool.class);
+	private final BlockingQueue<Session> sessionQueue;
+	private final BlockingQueue<Object> receive;
+	private final long timeout;
+
+	public ClientConnectionPool(int sessionSize, int retSize, long timeout) {
+		if (sessionSize > 0)
+			sessionQueue = new LinkedBlockingQueue<Session>(sessionSize);
+		else
+			sessionQueue = new LinkedBlockingQueue<Session>();
+
+		if (retSize > 0)
+			receive = new LinkedBlockingQueue<Object>(retSize);
+		else
+			receive = new LinkedBlockingQueue<Object>();
+
+		if (timeout > 0)
+			this.timeout = timeout;
+		else
+			this.timeout = 0;
+	}
+
+	public Session getSession() {
+		Session ret = null;
+		try {
+			if (timeout > 0) {
+				log.debug("get session timeout {}", timeout);
+				ret = sessionQueue.poll(timeout, TimeUnit.MILLISECONDS);
+			} else {
+				ret = sessionQueue.take();
+			}
+		} catch (InterruptedException e) {
+			log.error("get session error", e);
+		}
+		return ret;
+	}
+
+	public Session getSession(long timeout) {
+		Session ret = null;
+		try {
+			ret = sessionQueue.poll(timeout, TimeUnit.MILLISECONDS);
+		} catch (InterruptedException e) {
+			log.error("get session error", e);
+		}
+		return ret;
+	}
+
+	public void putSession(Session session) {
+		try {
+			if (timeout > 0) {
+				log.debug("put session timeout {}", timeout);
+				sessionQueue.offer(session, timeout, TimeUnit.MILLISECONDS);
+			} else {
+				sessionQueue.put(session);
+			}
+		} catch (InterruptedException e) {
+			log.error("put session error", e);
+		}
+	}
+
+	public void putSession(Session session, long timeout) {
+		try {
+			sessionQueue.offer(session, timeout, TimeUnit.MILLISECONDS);
+		} catch (InterruptedException e) {
+			log.error("put session error", e);
+		}
+	}
+
+	public Object getReceive() {
+		Object ret = null;
+		try {
+			if (timeout > 0) {
+				log.debug("get receive timeout {}", timeout);
+				ret = receive.poll(timeout, TimeUnit.MILLISECONDS);
+			} else {
+				ret = receive.take();
+			}
+		} catch (InterruptedException e) {
+			log.error("get session error", e);
+		}
+		return ret;
+	}
+
+	public Object getReceive(long timeout) {
+		Object ret = null;
+		try {
+			ret = receive.poll(timeout, TimeUnit.MILLISECONDS);
+		} catch (InterruptedException e) {
+			log.error("get session error", e);
+		}
+		return ret;
+	}
+
+	public void putReceive(Object object) {
+		try {
+			if (timeout > 0) {
+				log.debug("put receive timeout {}", timeout);
+				receive.offer(object, timeout, TimeUnit.MILLISECONDS);
+			} else {
+				receive.put(object);
+			}
+		} catch (InterruptedException e) {
+			log.error("put session error", e);
+		}
+	}
+
+	public void putReceive(Object object, long timeout) {
+		try {
+			receive.offer(object, timeout, TimeUnit.MILLISECONDS);
+		} catch (InterruptedException e) {
+			log.error("put session error", e);
+		}
+	}
+
+}
--- trunk/firefly-nettool/src/test/java/test/net/tcp/TestTcpCs.java
+++ trunk/firefly-nettool/src/test/java/test/net/tcp/TestTcpCs.java
@@ -1,13 +1,11 @@
 package test.net.tcp;
 
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
-
 import org.junit.Assert;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.firefly.net.Client;
+import com.firefly.net.ClientConnectionPool;
 import com.firefly.net.Handler;
 import com.firefly.net.Session;
 import com.firefly.net.tcp.TcpClient;
@@ -21,59 +19,59 @@ public class TestTcpCs {
 	public void testHello() {
 		new TcpServer("localhost", 9900, new StringLineDecoder(),
 				new StringLineEncoder(), new StringLineHandler()).start();
-		
+
 		try {
 			Thread.sleep(2000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		
-		final BlockingQueue<Session> queue = new LinkedBlockingQueue<Session>();
-		final BlockingQueue<String> receive = new LinkedBlockingQueue<String>();
-		Client client = new TcpClient(new StringLineDecoder(), new StringLineEncoder(), new Handler() {
 
-			@Override
-			public void sessionOpened(Session session) {
-				System.out.println("client session open |" + session.getSessionId());
-				queue.offer(session);
-			}
+		final ClientConnectionPool clientConnectionPool = new ClientConnectionPool(
+				1024, 1024, 1000);
+		Client client = new TcpClient(new StringLineDecoder(),
+				new StringLineEncoder(), new Handler() {
 
-			@Override
-			public void sessionClosed(Session session) {
-				System.out.println("client session close|" + session.getSessionId());
-			}
+					@Override
+					public void sessionOpened(Session session) {
+						System.out.println("client session open |"
+								+ session.getSessionId());
+						clientConnectionPool.putSession(session);
+					}
 
-			@Override
-			public void messageRecieved(Session session, Object message) {
-				String str = (String) message;
-				receive.offer(str);
-			}
+					@Override
+					public void sessionClosed(Session session) {
+						System.out.println("client session close|"
+								+ session.getSessionId());
+					}
 
-			@Override
-			public void exceptionCaught(Session session, Throwable t) {
-				System.out.println(t.getMessage() + "|"
-						+ session.getSessionId());
-			}
-		});
+					@Override
+					public void messageRecieved(Session session, Object message) {
+						String str = (String) message;
+						clientConnectionPool.putReceive(str);
+					}
+
+					@Override
+					public void exceptionCaught(Session session, Throwable t) {
+						System.out.println(t.getMessage() + "|"
+								+ session.getSessionId());
+					}
+				});
 		client.connect("localhost", 9900);
-		try {
-			Session session = queue.take();
-			session.encode("hello client");
-			String ret = receive.take();
-			log.info("receive[" + ret + "]");
-			Assert.assertThat(ret, is("hello client"));
-			
-			session.encode("test2");
-			ret = receive.take();
-			log.info("receive[" + ret + "]");
-			Assert.assertThat(ret, is("test2"));
-			
-			session.encode("quit");
-			ret = receive.take();
-			log.info("receive[" + ret + "]");
-			Assert.assertThat(ret, is("bye!"));
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+
+		Session session = clientConnectionPool.getSession();
+		session.encode("hello client");
+		String ret = (String) clientConnectionPool.getReceive();
+		log.info("receive[" + ret + "]");
+		Assert.assertThat(ret, is("hello client"));
+
+		session.encode("test2");
+		ret = (String) clientConnectionPool.getReceive();
+		log.info("receive[" + ret + "]");
+		Assert.assertThat(ret, is("test2"));
+
+		session.encode("quit");
+		ret = (String) clientConnectionPool.getReceive();
+		log.info("receive[" + ret + "]");
+		Assert.assertThat(ret, is("bye!"));
 	}
 }
