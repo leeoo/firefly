f7c3a4a371c8f1db0dd4c2c0512f062fd3fed542
baldulista@gmail.com <baldulista@gmail.com@4c6c975e-b6eb-aeeb-d668-919e69cb0141>
2011-06-11 04:01:17 +0000

    修改测试代码

--- firefly-nettool/src/test/java/test/net/tcp/FileTransferTcpServer.java
+++ firefly-nettool/src/test/java/test/net/tcp/FileTransferTcpServer.java
@@ -7,7 +7,7 @@ import com.firefly.net.tcp.TcpServer;
 public class FileTransferTcpServer {
 
     public static void main(String[] args) throws URISyntaxException {
-		System.out.println(SendFileHandler.class.getResource("/testFile.txt").toURI());
+//		System.out.println(SendFileHandler.class.getResource("/testFile.txt").toURI());
 
         new TcpServer(new StringLineDecoder(),
                 new StringLineEncoder(), new SendFileHandler()).start("localhost", 9900);
