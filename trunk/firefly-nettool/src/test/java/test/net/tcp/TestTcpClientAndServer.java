f2e370ccb08314a22c250bda64a7392e46096582
baldulista@gmail.com <baldulista@gmail.com@4c6c975e-b6eb-aeeb-d668-919e69cb0141>
2011-06-14 02:03:58 +0000

    up

--- trunk/firefly-nettool/src/test/java/test/net/tcp/TestTcpClientAndServer.java
+++ trunk/firefly-nettool/src/test/java/test/net/tcp/TestTcpClientAndServer.java
@@ -11,6 +11,8 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -18,6 +20,7 @@ import static org.hamcrest.Matchers.is;
 
 public class TestTcpClientAndServer {
     private static Logger log = LoggerFactory.getLogger(TestTcpClientAndServer.class);
+    private final Queue<Session> sessionPool = new ConcurrentLinkedQueue<Session>();
 
     @Test
     public void testHello() {
@@ -35,35 +38,38 @@ public class TestTcpClientAndServer {
         final StringLineClientHandler handler = new StringLineClientHandler(LOOP * 2);
         final Client client = new TcpClient(new StringLineDecoder(),
                 new StringLineEncoder(), handler);
+        for (int i = 0; i < LOOP; i++) {
+            final int sessionId = client.connect("localhost", 9900);
+            final Session session = handler.getSession(sessionId);
+            sessionPool.offer(session);
+        }
 
 
         for (int i = 0; i < LOOP; i++) {
             executorService.submit(new Runnable() {
                 @Override
                 public void run() {
-                    final int sessionId = client.connect("localhost", 9900);
-                    final Session session = handler.getSession(sessionId);
-
-		            session.encode("hello client");
+                    Session session = sessionPool.poll();
+                    session.encode("hello client");
                     log.debug("main thread {}", Thread.currentThread().toString());
-                    String ret = (String)session.getResult(1000);
+                    String ret = (String) session.getResult(1000);
                     log.debug("receive[" + ret + "]");
                     Assert.assertThat(ret, is("hello client"));
 
-		            session.encode("hello multithread test");
-                    ret = (String)session.getResult(1000);
+                    session.encode("hello multithread test");
+                    ret = (String) session.getResult(1000);
                     Assert.assertThat(ret, is("hello multithread test"));
 
-		            session.encode("getfile");
-                    ret = (String)session.getResult(1000);
+                    session.encode("getfile");
+                    ret = (String) session.getResult(1000);
                     log.debug("receive[" + ret + "]");
                     Assert.assertThat(ret, is("zero copy file transfers"));
 
-		            session.encode("quit");
-                    ret = (String)session.getResult(1000);
+                    session.encode("quit");
+                    ret = (String) session.getResult(1000);
                     log.debug("receive[" + ret + "]");
                     Assert.assertThat(ret, is("bye!"));
-                    log.debug("complete session {}", sessionId);
+                    log.debug("complete session {}", session.getSessionId());
                 }
             });
 
@@ -72,14 +78,14 @@ public class TestTcpClientAndServer {
         final int sessionId = client.connect("localhost", 9900);
         final Session session = handler.getSession(sessionId);
 
-		session.encode("hello client 2");
+        session.encode("hello client 2");
         log.debug("main thread {}", Thread.currentThread().toString());
-        String ret = (String)session.getResult(1000);
+        String ret = (String) session.getResult(1000);
         log.debug("receive[" + ret + "]");
         Assert.assertThat(ret, is("hello client 2"));
 
-		session.encode("quit");
-        ret = (String)session.getResult(1000);
+        session.encode("quit");
+        ret = (String) session.getResult(1000);
         log.debug("receive[" + ret + "]");
         Assert.assertThat(ret, is("bye!"));
         log.debug("complete session {}", sessionId);
