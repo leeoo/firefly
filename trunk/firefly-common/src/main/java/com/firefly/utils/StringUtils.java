c902af96a53407650c13b37e79338790a6157594
baldulista@gmail.com <baldulista@gmail.com@4c6c975e-b6eb-aeeb-d668-919e69cb0141>
2011-06-16 16:03:31 +0000

    up

--- trunk/firefly-common/src/main/java/com/firefly/utils/StringUtils.java
+++ trunk/firefly-common/src/main/java/com/firefly/utils/StringUtils.java
@@ -2,6 +2,7 @@ package com.firefly.utils;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class StringUtils {
 
@@ -342,5 +343,29 @@ public class StringUtils {
 
 	public static boolean hasLength(String str) {
 		return hasLength((CharSequence) str);
+	}
+	
+	private static final String START_FLAG = "${";
+	private static final String END_FLAG = "}";
+	/**
+	 * 将字符串中特定模式的字符转换成map中对应的值
+	 *
+	 * @param s
+	 *            需要转换的字符串
+	 * @param map
+	 *            转换所需的键值对集合
+	 * @return 转换后的字符串
+	 */
+	public static String replace(String s, Map<String, String> map) {
+		StringBuilder ret = new StringBuilder(s.length());
+		int cursor = 0;
+		for (int start, end; (start = s.indexOf(START_FLAG, cursor)) != -1
+				&& (end = s.indexOf(END_FLAG, start)) != -1;) {
+			ret.append(s.substring(cursor, start)).append(
+					map.get(s.substring(start + START_FLAG.length(), end)));
+			cursor = end + END_FLAG.length();
+		}
+		ret.append(s.substring(cursor, s.length()));
+		return ret.toString();
 	}
 }
