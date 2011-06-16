c2630d4606ba4c513dc5b4dd9365e38fc56b5ec3
baldulista@gmail.com <baldulista@gmail.com@4c6c975e-b6eb-aeeb-d668-919e69cb0141>
2011-06-16 16:52:43 +0000

    up

---
+++ trunk/firefly-common/src/main/java/com/firefly/utils/log/LogFactory.java
@@ -0,0 +1,7 @@
+package com.firefly.utils.log;
+
+public class LogFactory {
+	public static Log getLog(String str) {
+		return null;
+	}
+}
---
+++ trunk/firefly-common/src/main/java/com/firefly/utils/log/file/FileLog.java
@@ -0,0 +1,69 @@
+package com.firefly.utils.log.file;
+
+import com.firefly.utils.log.Log;
+
+public class FileLog implements Log {
+
+	@Override
+	public void trace(String str, Object... obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void trace(String str, Throwable throwable, Object... obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void debug(String str, String... obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void debug(String str, Throwable throwable, Object... obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void info(String str, String... obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void info(String str, Throwable throwable, Object... obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void warn(String str, String... obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void warn(String str, Throwable throwable, Object... obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void error(String str, String... obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void error(String str, Throwable throwable, Object... obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+}
