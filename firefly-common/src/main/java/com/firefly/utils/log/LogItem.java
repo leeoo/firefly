52bf6e50037f2fee967a664d5b76ca59802620c1
baldulista@gmail.com <baldulista@gmail.com@4c6c975e-b6eb-aeeb-d668-919e69cb0141>
2011-06-21 13:39:05 +0000

    up

--- firefly-common/src/main/java/com/firefly/utils/log/LogItem.java
+++ firefly-common/src/main/java/com/firefly/utils/log/LogItem.java
@@ -1,5 +1,30 @@
 package com.firefly.utils.log;
 
 public class LogItem {
-    
+	private String name, content, data;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
 }
