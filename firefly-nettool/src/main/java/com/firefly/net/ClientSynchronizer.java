4b42d0f155d209440a89052a8a90d493b5de53c1
baldulista@gmail.com <baldulista@gmail.com@4c6c975e-b6eb-aeeb-d668-919e69cb0141>
2011-07-13 04:48:13 +0000

    up

--- firefly-nettool/src/main/java/com/firefly/net/ClientSynchronizer.java
+++ firefly-nettool/src/main/java/com/firefly/net/ClientSynchronizer.java
@@ -4,57 +4,53 @@ import com.firefly.utils.log.Log;
 import com.firefly.utils.log.LogFactory;
 
 public class ClientSynchronizer<T> {
-    private static Log log = LogFactory.getInstance().getLog("firefly-system");
-    private SynchronousObject<T>[] objs;
-    private final long timeout;
-    private int size;
-    
-    public ClientSynchronizer() {
-    	this(0, 0);
-    }
+	private static Log log = LogFactory.getInstance().getLog("firefly-system");
+	private SynchronousObject<T>[] objs;
+	private final long timeout;
+	private int size;
 
-    public ClientSynchronizer(int size, long timeout) {
-        if (size <= 0)
-            size = 1024 * 16;
-        
-        int i = 2;
-        while(i < size)
-        	i <<= 1;
+	public ClientSynchronizer() {
+		this(0, 0);
+	}
 
-        this.size = i;
-        log.info("synchronizer size: {}", this.size);
+	public ClientSynchronizer(int size, long timeout) {
+		if (size <= 0) {
+			this.size = 1024 * 16;
+		} else {
+			int i = 2;
+			while (i < size)
+				i <<= 1;
 
-        if (timeout > 0)
-            this.timeout = timeout;
-        else
-            this.timeout = 5000;
+			this.size = i;
+		}
+		log.info("synchronizer size: {}", this.size);
+		this.timeout = timeout > 0 ? timeout : 5000;
+		init();
+		log.debug("client timeout {}", timeout);
+	}
 
-        init();
-        log.debug("client timeout {}", timeout);
-    }
+	public T get(int index) {
+		log.debug("get index {}", index);
+		return objs[index & (size - 1)].get(timeout);
+	}
 
-    public T get(int index) {
-        log.debug("get index {}", index);
-        return objs[index & (size - 1)].get(timeout);
-    }
+	public void put(T t, int index) {
+		log.debug("put index {}", index);
+		objs[index & (size - 1)].put(t);
+	}
 
-    public void put(T t, int index) {
-        log.debug("put index {}", index);
-        objs[index & (size - 1)].put(t);
-    }
-    
-    public void reset(int index) {
-    	objs[index & (size - 1)].reset();
-    }
+	public void reset(int index) {
+		objs[index & (size - 1)].reset();
+	}
 
-    @SuppressWarnings("unchecked")
+	@SuppressWarnings("unchecked")
 	public void init() {
-        objs = new SynchronousObject[size];
+		objs = new SynchronousObject[size];
 
-        for (int i = 0; i < objs.length; i++) {
-            objs[i] = new SynchronousObject<T>();
-        }
+		for (int i = 0; i < objs.length; i++) {
+			objs[i] = new SynchronousObject<T>();
+		}
 
-    }
+	}
 
 }
