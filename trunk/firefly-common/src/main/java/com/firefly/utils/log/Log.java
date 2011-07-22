6a9f86abc02ae37ac7d06595b592fc5cf3288ad6
baldulista@gmail.com <baldulista@gmail.com@4c6c975e-b6eb-aeeb-d668-919e69cb0141>
2011-07-22 09:28:59 +0000

    up

--- trunk/firefly-common/src/main/java/com/firefly/utils/log/Log.java
+++ trunk/firefly-common/src/main/java/com/firefly/utils/log/Log.java
@@ -7,30 +7,40 @@ public interface Log {
 	int WARN = 3;
 	int ERROR = 4;
 	String CL = "\r\n";
+	
+	boolean isTraceEnable();
 
 	void trace(String str);
 	
 	void trace(String str, Object... objs);
 
 	void trace(String str, Throwable throwable, Object... objs);
+	
+	boolean isDebugEnable();
 
 	void debug(String str);
 	
 	void debug(String str, Object... objs);
 
 	void debug(String str, Throwable throwable, Object... objs);
+	
+	boolean isInfoEnable();
 
 	void info(String str);
 	
 	void info(String str, Object... objs);
 
 	void info(String str, Throwable throwable, Object... objs);
+	
+	boolean isWarnEnable();
 
 	void warn(String str);
 	
 	void warn(String str, Object... objs);
 
 	void warn(String str, Throwable throwable, Object... objs);
+	
+	boolean isErrorEnable();
 
 	void error(String str);
 	
