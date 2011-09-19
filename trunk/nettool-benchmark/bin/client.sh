6f34a5438b5e16ae26763e0431fc23c0a73bd8e5
baldulista@gmail.com <baldulista@gmail.com@4c6c975e-b6eb-aeeb-d668-919e69cb0141>
2011-09-19 04:51:10 +0000

    更新nettool性能测试代码，测试前创建好连接

--- trunk/nettool-benchmark/bin/client.sh
+++ trunk/nettool-benchmark/bin/client.sh
@@ -2,7 +2,7 @@ home=/usr/local/app/alvinqiu/perf
 classpath=$home/classes`find $home/lib -iname *.jar | xargs -n 1 -i echo -n :{}`
 mainclass=test.StringLinePerformance
 jdk=/usr/local/jdk/bin/java
-jvm_params="-Dbind_host=10.147.22.163 -Dbind_port=9900 -Dloop=2000 -Dthread_num=500 -Dasyn=0 -Xms1024m -Xmx1024m -Xss1m -XX:NewSize=256m -XX:MaxNewSize=256m -XX:+UseCompressedOops -XX:+UseConcMarkSweepGC -XX:+DisableExplicitGC -XX:PermSize=64m -XX:MaxPermSize=64m"
+jvm_params="-Dbind_host=10.147.22.162 -Dbind_port=9900 -Dloop=2000 -Dthread_num=500 -Dasyn=0 -Xms1024m -Xmx1024m -Xss1m -XX:NewSize=256m -XX:MaxNewSize=256m -XX:+UseCompressedOops -XX:+UseConcMarkSweepGC -XX:+DisableExplicitGC -XX:PermSize=64m -XX:MaxPermSize=64m"
 stderr=$home/log/stderr.log
 stdout=$home/log/stdout.log
 command="nohup $jdk $jvm_params -cp $classpath $mainclass $*"
