d4298e2156aa0e562734c127dce91c4f90528007
baldulista@gmail.com <baldulista@gmail.com@4c6c975e-b6eb-aeeb-d668-919e69cb0141>
2011-09-19 04:49:42 +0000

    更新nettool性能测试代码，测试前创建好连接

--- nettool-benchmark/bin/server.sh
+++ nettool-benchmark/bin/server.sh
@@ -2,9 +2,10 @@ home=/usr/local/app/alvinqiu/perf
 classpath=$home/classes`find $home/lib -iname *.jar | xargs -n 1 -i echo -n :{}`
 mainclass=test.StringLineTcpServer
 jdk=/usr/local/jdk/bin/java
+jvm_params="-Dbind_host=10.147.22.162 -Dbind_port=9900 -Xms1024m -Xmx1024m -Xss1m -XX:NewSize=256m -XX:MaxNewSize=256m -XX:+UseCompressedOops -XX:+UseConcMarkSweepGC -XX:+DisableExplicitGC -XX:PermSize=64m -XX:MaxPermSize=64m"
 stderr=$home/log/stderr.log
 stdout=$home/log/stdout.log
-command="nohup $jdk -cp $classpath $mainclass $*"
+command="nohup $jdk $jvm_params -cp $classpath $mainclass $*"
 echo 'started...'
 echo $command
 $command >> $stdout 2>> $stderr &
