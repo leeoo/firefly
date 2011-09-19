6cf9e6263f5dc03828a8d8c75571247dd9ad788f
baldulista@gmail.com <baldulista@gmail.com@4c6c975e-b6eb-aeeb-d668-919e69cb0141>
2011-09-19 17:49:48 +0000

    up

--- trunk/nettool-benchmark/src/test/StringLinePerformance.java
+++ trunk/nettool-benchmark/src/test/StringLinePerformance.java
@@ -15,6 +15,7 @@ public class StringLinePerformance {
 	public static int LOOP;
 	public static int THREAD;
 	public static TcpConnection[] tcpConnections;
+	public static String DATA;
 
 	public static class ClientSynchronizeTask implements Runnable {
 
@@ -25,8 +26,8 @@ public class StringLinePerformance {
 		public void run() {
 			TcpConnection c = tcpConnections[id];
 			for (int i = 0; i < LOOP; i++) {
-				String message = "hello world! " + c.getId();
-				String ret = (String) c.send(message);
+//				String message = "hello world! " + c.getId();
+				String ret = (String) c.send(DATA);
 				log.debug("rev: {}", ret);
 			}
 			c.close(false);
@@ -57,8 +58,8 @@ public class StringLinePerformance {
 		public void run() {
 			TcpConnection c = tcpConnections[id];
 			for (int i = 0; i < LOOP; i++) {
-				String message = "hello world! " + c.getId();
-				c.send(message, new MessageReceiveCallBack() {
+//				String message = "hello world! " + c.getId();
+				c.send(DATA, new MessageReceiveCallBack() {
 
 					@Override
 					public void messageRecieved(Session session, Object obj) {
@@ -107,27 +108,35 @@ public class StringLinePerformance {
 			int reqs = LOOP * THREAD;
 
 			double throughput = (reqs / (double) time) * 1000;
-			log.info("throughput: {}", throughput);
+			log.info("throughput: {} req/s, {} KBytes/s", throughput, THREAD * LOOP * DATA.length() * 2L * 1000 / 1024.0 / time);
 		}
 
 	}
 
 	public static void main(String[] args) {
-//		System.setProperty("bind_host", "localhost");
-//		System.setProperty("bind_port", "9900");
-//		System.setProperty("loop", "2000");
-//		System.setProperty("thread_num", "500");
-//		System.setProperty("asyn", "1");
+		System.setProperty("bind_host", "127.0.0.1");
+		System.setProperty("bind_port", "9900");
+		System.setProperty("loop", "2000");
+		System.setProperty("thread_num", "500");
+		System.setProperty("asyn", "0");
+		System.setProperty("size", "4096");
 		
 		String host = System.getProperty("bind_host");
 		int port = Integer.parseInt(System.getProperty("bind_port"));
 		int asyn = Integer.parseInt(System.getProperty("asyn"));
+		int size = Integer.parseInt(System.getProperty("size"));
 
 		LOOP = Integer.parseInt(System.getProperty("loop"));
 		THREAD = Integer.parseInt(System.getProperty("thread_num"));
 		tcpConnections = new TcpConnection[THREAD];
-		log.info("threads: {}, loop: {}, total request: {}", THREAD, LOOP,
-				THREAD * LOOP);
+		log.info("threads: {}, loop: {}, total request: {}, total transferred: {}", THREAD, LOOP,
+				THREAD * LOOP, THREAD * LOOP * (long)size);
+		byte[] data = new byte[size / 2];
+		for (int i = 0; i < data.length; i++) {
+			data[i] = 80;
+		}
+		DATA = new String(data);
+		
 
 		ExecutorService executorService = Executors.newFixedThreadPool(THREAD);
 		final SimpleTcpClient client = new SimpleTcpClient(host, port,
