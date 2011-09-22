b0bf0cdfdfc75de6c81f7736312b4d8e162eee36
baldulista@gmail.com <baldulista@gmail.com@4c6c975e-b6eb-aeeb-d668-919e69cb0141>
2011-09-22 16:07:23 +0000

    up

--- trunk/nettool-benchmark/src/test/StringLineTcpServer.java
+++ trunk/nettool-benchmark/src/test/StringLineTcpServer.java
@@ -7,8 +7,8 @@ import com.firefly.net.tcp.TcpServer;
 public class StringLineTcpServer {
 
     public static void main(String[] args) {
-//    	System.setProperty("bind_host", "127.0.0.1");
-//    	System.setProperty("bind_port", "9900");
+    	System.setProperty("bind_host", "127.0.0.1");
+    	System.setProperty("bind_port", "9900");
     	
     	String host = System.getProperty("bind_host");
     	int port = Integer.parseInt(System.getProperty("bind_port"));
