a95587b2c63d889f4e292ae9d368f2a7de9d800d
baldulista@gmail.com <baldulista@gmail.com@4c6c975e-b6eb-aeeb-d668-919e69cb0141>
2011-09-22 01:37:22 +0000

    更新测试用例

--- firefly-nettool/src/test/java/test/net/tcp/TestTcpClientAndServer.java
+++ firefly-nettool/src/test/java/test/net/tcp/TestTcpClientAndServer.java
@@ -2,8 +2,6 @@ package test.net.tcp;
 
 import com.firefly.net.Config;
 import com.firefly.net.Server;
-import com.firefly.net.Session;
-import com.firefly.net.support.MessageReceiveCallBack;
 import com.firefly.net.support.StringLineDecoder;
 import com.firefly.net.support.StringLineEncoder;
 import com.firefly.net.support.TcpConnection;
@@ -43,23 +41,12 @@ public class TestTcpClientAndServer {
 				public void run() {
 					final TcpConnection c = client.connect();
 					Assert.assertThat(c.isOpen(), is(true));
-
 					log.debug("main thread {}", Thread.currentThread()
 							.toString());
-					String ret = (String) c.send("hello client");
-					log.debug("receive[" + ret + "]");
-					Assert.assertThat(ret, is("hello client"));
-
-					ret = (String) c.send("hello multithread test");
-					Assert.assertThat(ret, is("hello multithread test"));
-
-					ret = (String) c.send("getfile");
-					log.debug("receive[" + ret + "]");
-					Assert.assertThat(ret, is("zero copy file transfers"));
-
-					ret = (String) c.send("quit");
-					log.debug("receive[" + ret + "]");
-					Assert.assertThat(ret, is("bye!"));
+					Assert.assertThat((String) c.send("hello client"), is("hello client"));
+					Assert.assertThat((String) c.send("hello multithread test"), is("hello multithread test"));
+					Assert.assertThat((String) c.send("getfile"), is("zero copy file transfers"));
+					Assert.assertThat((String) c.send("quit"), is("bye!"));
 					log.debug("complete session {}", c.getId());
 				}
 			});
@@ -67,25 +54,7 @@ public class TestTcpClientAndServer {
 		}
 
 		final TcpConnection c = client.connect();
-
-		log.debug("main thread {}", Thread.currentThread().toString());
-		c.send("hello client 2", new MessageReceiveCallBack() {
-
-			@Override
-			public void messageRecieved(Session session, Object obj) {
-				Assert.assertThat((String) obj, is("hello client 2"));
-
-			}
-		});
-
-		c.send("quit", new MessageReceiveCallBack() {
-
-			@Override
-			public void messageRecieved(Session session, Object obj) {
-				Assert.assertThat((String) obj, is("bye!"));
-				log.debug("complete session {}", c.getId());
-			}
-		});
-
+		Assert.assertThat((String) c.send("hello client 2"), is("hello client 2"));
+		Assert.assertThat((String) c.send("quit"), is("bye!"));
 	}
 }
