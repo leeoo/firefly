e06cf89bbb62ff9f7a583e0d643c1fe5d291e501
baldulista@gmail.com <baldulista@gmail.com@4c6c975e-b6eb-aeeb-d668-919e69cb0141>
2011-09-23 05:02:36 +0000

    简化TimeWheel代码

--- trunk/firefly-common/src/main/java/com/firefly/utils/time/wheel/TimeWheel.java
+++ trunk/firefly-common/src/main/java/com/firefly/utils/time/wheel/TimeWheel.java
@@ -1,22 +1,13 @@
 package com.firefly.utils.time.wheel;
 
 import java.util.Iterator;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.TimeUnit;
 
 public class TimeWheel {
 	private Config config = new Config();
 	private TimerSlot[] timerSlots;
 	private int currentSlot = 0;
 	private volatile int currentSlotNum = 0;
-	private ExecutorService workerThreadPool;
 	private volatile boolean start;
-	private ScheduledFuture<?> fireHandle;
-	private ScheduledExecutorService scheduler;
 
 	public Config getConfig() {
 		return config;
@@ -47,9 +38,6 @@ public class TimeWheel {
 		if (index <= currentSlotNum && round > 0) {
 			round -= 1;
 		}
-		// System.out.println("maxTimers: " + maxTimers);
-		// System.out.println("currentSlotNum: " + currentSlotNum + " index: " +
-		// index + " round: " + round);
 
 		TimerNode node = new TimerNode();
 		node.setRound(round);
@@ -70,58 +58,49 @@ public class TimeWheel {
 						timerSlot.setSlotNum(i);
 						timerSlots[i] = timerSlot;
 					}
-					final boolean hasWorkers = config.getWorkerThreads() > 0;
-					if (hasWorkers)
-						workerThreadPool = Executors.newFixedThreadPool(config
-								.getWorkerThreads());
-
-					scheduler = Executors
-							.newSingleThreadScheduledExecutor(new ThreadFactory() {
-								@Override
-								public Thread newThread(Runnable r) {
-									return new Thread(r, "firefly time wheel");
-								}
-							});
-
-					fireHandle = scheduler.scheduleAtFixedRate(new Runnable() {
-
-						@Override
-						public void run() {
-							TimerSlot timerSlot = timerSlots[currentSlot++];
-							currentSlotNum = timerSlot.getSlotNum();
-//							System.out.println("fire: " + timerSlot.getQueue().size());
-							for (Iterator<TimerNode> iterator = timerSlot
-									.getQueue().iterator(); iterator.hasNext();) {
-								TimerNode node = iterator.next();
-								if (node.getRound() == 0) {
-									if (hasWorkers)
-										workerThreadPool.submit(node.getRun());
-									else
-										node.getRun().run();
-									iterator.remove();
-								} else {
-									node.setRound(node.getRound() - 1);
-								}
-							}
-
-							if (currentSlot == timerSlots.length)
-								currentSlot = 0;
-						}
-					}, 0, config.getInterval(), TimeUnit.MILLISECONDS);
 
 					start = true;
-
+					new Thread(new Worker(), "firefly time wheel").start();
 				}
 			}
 		}
 	}
 
 	public void stop() {
-		if (workerThreadPool != null)
-			workerThreadPool.shutdown();
-		fireHandle.cancel(true);
-		scheduler.shutdown();
 		start = false;
+	}
+	
+	private final class Worker implements Runnable {
+
+		@Override
+		public void run() {
+			while(start) {
+				TimerSlot timerSlot = timerSlots[currentSlot++];
+				currentSlotNum = timerSlot.getSlotNum();
+
+				for (Iterator<TimerNode> iterator = timerSlot
+						.getQueue().iterator(); iterator.hasNext();) {
+					TimerNode node = iterator.next();
+					if (node.getRound() == 0) {
+						node.getRun().run();
+						iterator.remove();
+					} else {
+						node.setRound(node.getRound() - 1);
+					}
+				}
+
+				if (currentSlot == timerSlots.length)
+					currentSlot = 0;
+				
+				try {
+					Thread.sleep(config.getInterval());
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			
+		}
+		
 	}
 
 }
