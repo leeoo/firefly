8f4a54198ca7bb6dc9e5b95cd82ed667c930edc4
baldulista@gmail.com <baldulista@gmail.com@4c6c975e-b6eb-aeeb-d668-919e69cb0141>
2011-10-24 16:23:42 +0000

    编写模版分析程序

--- trunk/firefly-template/src/main/java/com/firefly/template/parser/JavaFileBuilder.java
+++ trunk/firefly-template/src/main/java/com/firefly/template/parser/JavaFileBuilder.java
@@ -1,33 +1,50 @@
 package com.firefly.template.parser;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import com.firefly.template.Config;
 
 public class JavaFileBuilder {
 	private String name;
-	private StringBuilder content;
+	private BufferedWriter writer;
 	private Config config;
 
-	public JavaFileBuilder(Config config) {
+	public JavaFileBuilder(Config config, String name) {
 		this.config = config;
+		this.name = name;
+		File file = new File(config.getCompiledPath(), name);
+		try {
+			if (!file.exists())
+				file.createNewFile();
+			
+			writer = new BufferedWriter(new FileWriter(file));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public String getName() {
 		return name;
 	}
-
-	public void setName(String name) {
-		this.name = name;
+	
+	public JavaFileBuilder append(String str) {
+		try {
+			writer.write(str);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return this;
 	}
 
-	public StringBuilder append(String string) {
-		return content.append(string);
-	}
-
-	public void save() {
-		File file = new File(config.getCompiledPath(), name);
-		// TODO 
-//		if(!file.exists())
+	public void close() {
+		try {
+			if(writer != null)
+				writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
