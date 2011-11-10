1708b5c6b5a368aeb707d81e64e86e60f452b697
baldulista@gmail.com <baldulista@gmail.com@4c6c975e-b6eb-aeeb-d668-919e69cb0141>
2011-11-10 18:30:23 +0000

    更新模版文件读取方式

--- trunk/firefly-template/src/main/java/com/firefly/template/parser/ViewFileReader.java
+++ trunk/firefly-template/src/main/java/com/firefly/template/parser/ViewFileReader.java
@@ -1,10 +1,8 @@
 package com.firefly.template.parser;
 
-import java.io.BufferedReader;
+import java.io.Closeable;
 import java.io.File;
 import java.io.FileFilter;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,6 +10,8 @@ import java.util.List;
 import com.firefly.template.Config;
 import com.firefly.template.exception.TemplateFileReadException;
 import com.firefly.template.support.CompileUtils;
+import com.firefly.utils.io.FileUtils;
+import com.firefly.utils.io.LineReaderHandler;
 
 public class ViewFileReader {
 	private Config config;
@@ -79,68 +79,17 @@ public class ViewFileReader {
 		classNames.add(name.substring(0, name.length() - 5));
 		javaFiles.add(config.getCompiledPath() + "/" + name);
 		System.out.println("======= " + name + " =======");
+		
 		JavaFileBuilder javaFileBuilder = new JavaFileBuilder(
 				config.getCompiledPath(), name, config);
-		BufferedReader reader = null;
-		StringBuilder text = new StringBuilder();
-		StringBuilder comment = new StringBuilder();
-		int status = 0;
+		TemplateFileLineReaderHandler lineReaderHandler = new TemplateFileLineReaderHandler(javaFileBuilder);
+		
 		try {
-			reader = new BufferedReader(new FileReader(f));
-			for (String line = null; (line = reader.readLine()) != null;) {
-				switch (status) {
-				case 0:
-					int i = line.indexOf("<!--");
-
-					if (i >= 0) { // html注释开始
-						text.append(line.substring(0, i).trim());
-						if (text.length() > 0) {
-							parseText(text.toString(), javaFileBuilder);
-							text = new StringBuilder();
-						}
-
-						int j = line.indexOf("-->");
-						if (j > i + 4) { // html注释结束
-							assert comment.length() == 0;
-							parseComment(line.substring(i + 4, j).trim(),
-									javaFileBuilder);
-						} else {
-							status = 1;
-							comment.append(line.substring(i + 4).trim() + "\n");
-						}
-					} else {
-						text.append(line.trim());
-					}
-					break;
-				case 1:
-					int j = line.indexOf("-->");
-					if (j >= 0) { // html注释结束
-						status = 0;
-						comment.append(line.substring(0, j).trim());
-						parseComment(comment.toString(), javaFileBuilder);
-						comment = new StringBuilder();
-					} else
-						comment.append(line.trim() + "\n");
-					break;
-				}
-			}
-			if (text.length() > 0) {
-				parseText(text.toString(), javaFileBuilder);
-			}
-
-			javaFileBuilder.write("\t}\n\n").writeTail().write("}");
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
+			FileUtils.read(f, lineReaderHandler);
 		} catch (IOException e) {
 			e.printStackTrace();
 		} finally {
-			if (reader != null)
-				try {
-					reader.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			javaFileBuilder.close();
+			lineReaderHandler.close();
 		}
 	}
 
@@ -153,9 +102,71 @@ public class ViewFileReader {
 		for (int start, end; (start = text.indexOf("${", cursor)) != -1
 				&& (end = text.indexOf("}", start)) != -1;) {
 			javaFileBuilder.writeText(text.substring(cursor, start))
-			.writeObject(text.substring(start + 2, end));
+					.writeObject(text.substring(start + 2, end));
 			cursor = end + 1;
 		}
 		javaFileBuilder.writeText(text.substring(cursor, text.length()));
+	}
+	
+	private class TemplateFileLineReaderHandler implements LineReaderHandler, Closeable {
+		JavaFileBuilder javaFileBuilder;
+		StringBuilder text = new StringBuilder();
+		StringBuilder comment = new StringBuilder();
+		int status = 0;
+		
+		public TemplateFileLineReaderHandler(JavaFileBuilder javaFileBuilder) {
+			this.javaFileBuilder = javaFileBuilder;
+		}
+		
+		@Override
+		public void close() {
+			if (text.length() > 0) {
+				parseText(text.toString(), javaFileBuilder);
+			}
+
+			javaFileBuilder.write("\t}\n\n").writeTail().write("}");
+			javaFileBuilder.close();
+		}
+
+		@Override
+		public void readline(String line, int num) {
+			switch (status) {
+			case 0:
+				int i = line.indexOf("<!--");
+
+				if (i >= 0) { // html注释开始
+					text.append(line.substring(0, i).trim());
+					if (text.length() > 0) {
+						parseText(text.toString(), javaFileBuilder);
+						text = new StringBuilder();
+					}
+
+					int j = line.indexOf("-->");
+					if (j > i + 4) { // html注释结束
+						assert comment.length() == 0;
+						parseComment(line.substring(i + 4, j).trim(),
+								javaFileBuilder);
+					} else {
+						status = 1;
+						comment.append(line.substring(i + 4).trim()
+								+ "\n");
+					}
+				} else {
+					text.append(line.trim());
+				}
+				break;
+			case 1:
+				int j = line.indexOf("-->");
+				if (j >= 0) { // html注释结束
+					status = 0;
+					comment.append(line.substring(0, j).trim());
+					parseComment(comment.toString(), javaFileBuilder);
+					comment = new StringBuilder();
+				} else
+					comment.append(line.trim() + "\n");
+				break;
+			}
+
+		}
 	}
 }
