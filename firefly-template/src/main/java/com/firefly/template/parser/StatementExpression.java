78be6e92a93886da07a252779e411b20ecc7c477
baldulista@gmail.com <baldulista@gmail.com@4c6c975e-b6eb-aeeb-d668-919e69cb0141>
2011-11-16 15:30:08 +0000

    优化代码结构

--- firefly-template/src/main/java/com/firefly/template/parser/StatementExpression.java
+++ firefly-template/src/main/java/com/firefly/template/parser/StatementExpression.java
@@ -1,5 +1,7 @@
 package com.firefly.template.parser;
 
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
 
 import com.firefly.template.support.RPNUtils;
@@ -10,8 +12,13 @@ public class StatementExpression implements Statement {
 	@Override
 	public void parse(String content, JavaFileBuilder javaFileBuilder) {
 		List<Fragment> list = RPNUtils.getReversePolishNotation(content);
+		Deque<Fragment> valDeque = new LinkedList<Fragment>();
 		for(Fragment f : list) {
-			
+			if(f.symbol) {
+				
+			} else {
+				
+			}
 		}
 	}
 
