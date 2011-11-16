cbf6b17e29c62cab379e6f37aca83457026827c8
baldulista@gmail.com <baldulista@gmail.com@4c6c975e-b6eb-aeeb-d668-919e69cb0141>
2011-11-16 15:10:10 +0000

    修改结构

--- firefly-template/src/main/java/com/firefly/template/parser/StatementExpression.java
+++ firefly-template/src/main/java/com/firefly/template/parser/StatementExpression.java
@@ -1,10 +1,18 @@
 package com.firefly.template.parser;
 
+import java.util.List;
+
+import com.firefly.template.support.RPNUtils;
+import com.firefly.template.support.RPNUtils.Fragment;
+
 public class StatementExpression implements Statement {
 
 	@Override
 	public void parse(String content, JavaFileBuilder javaFileBuilder) {
-		
+		List<Fragment> list = RPNUtils.getReversePolishNotation(content);
+		for(Fragment f : list) {
+			
+		}
 	}
 
 }
--- firefly-template/src/main/java/com/firefly/template/support/ObjectMetaInfoCache.java
+++ firefly-template/src/main/java/com/firefly/template/support/ObjectMetaInfoCache.java
@@ -4,22 +4,18 @@ import java.lang.reflect.Method;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
-import com.firefly.template.ClassCache;
 
-
-public class ObjectMetaInfoCache implements ClassCache {
+public class ObjectMetaInfoCache {
 	private Map<String, Method> map;
 
 	public ObjectMetaInfoCache() {
 		map = new ConcurrentHashMap<String, Method>();
 	}
 
-	@Override
 	public Method get(Class<?> clazz, String propertyName) {
 		return map.get(clazz.getName() + "#" + propertyName);
 	}
 
-	@Override
 	public void put(Class<?> clazz, String propertyName, Method method) {
 		map.put(clazz.getName() + "#" + propertyName, method);
 	}
