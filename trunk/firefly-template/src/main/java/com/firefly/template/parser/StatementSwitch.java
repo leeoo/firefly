0bf3b8a942b36b437a9b093cbaaeae88bcd9a619
baldulista@gmail.com <baldulista@gmail.com@4c6c975e-b6eb-aeeb-d668-919e69cb0141>
2011-11-25 08:23:47 +0000

    增加switch, case, default语法

---
+++ trunk/firefly-template/src/main/java/com/firefly/template/parser/StatementSwitch.java
@@ -0,0 +1,20 @@
+package com.firefly.template.parser;
+
+public class StatementSwitch implements Statement {
+
+	@Override
+	public void parse(String content, JavaFileBuilder javaFileBuilder) {
+		javaFileBuilder.outBreak = false;
+		String obj = content.substring(content.indexOf("${") + 2, content.length() - 1);
+		javaFileBuilder.write(javaFileBuilder.getPreBlank() + "switch(objNav.getInteger(model, \"" + obj + "\")) {\n");
+//		switch (key) {
+//		case value:
+//			
+//			break;
+//
+//		default:
+//			break;
+//		}
+	}
+
+}
