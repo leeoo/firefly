4de82c087cde26cbdbbeffe5547f573ff5006235
baldulista@gmail.com <baldulista@gmail.com@4c6c975e-b6eb-aeeb-d668-919e69cb0141>
2011-12-21 16:38:28 +0000

    默认改成fft视图

--- firefly-hello/src/main/java/com/test/sample/controller/HelloServlet.java
+++ firefly-hello/src/main/java/com/test/sample/controller/HelloServlet.java
@@ -14,6 +14,7 @@ public class HelloServlet extends HttpServlet {
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		System.out.println(request.getContextPath());
 		request.setAttribute("hello", "你好 firefly!");
 		request.getRequestDispatcher("/WEB-INF/page/index.jsp").forward(
 				request, response);
