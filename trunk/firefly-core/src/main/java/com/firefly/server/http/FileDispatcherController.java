0f55a44c4c857d7b18f1594ba0dc6ef8da1ab5f6
baldulista@gmail.com <baldulista@gmail.com@4c6c975e-b6eb-aeeb-d668-919e69cb0141>
2012-02-12 15:24:10 +0000

    增加静态页面输出

--- trunk/firefly-core/src/main/java/com/firefly/server/http/FileDispatcherController.java
+++ trunk/firefly-core/src/main/java/com/firefly/server/http/FileDispatcherController.java
@@ -1,9 +1,15 @@
 package com.firefly.server.http;
 
+import java.io.File;
+import java.io.IOException;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.firefly.mvc.web.DispatcherController;
+import com.firefly.mvc.web.servlet.SystemHtmlPage;
+import com.firefly.server.exception.HttpServerException;
+import com.firefly.server.io.StaticFileOutputStream;
 import com.firefly.utils.log.Log;
 import com.firefly.utils.log.LogFactory;
 
@@ -11,18 +17,40 @@ public class FileDispatcherController implements DispatcherController {
 
 	private static Log log = LogFactory.getInstance().getLog("firefly-system");
 	private Config config;
-	
+
 	public FileDispatcherController(Config config) {
 		this.config = config;
 	}
 
-
-
 	@Override
 	public void dispatcher(HttpServletRequest request,
 			HttpServletResponse response) {
-		// TODO Auto-generated method stub
-
+		File file = new File(config.getServerHome(), request.getRequestURI());
+		if (file.exists()) {
+			response.setContentType("text/html; charset="
+					+ config.getEncoding());
+			StaticFileOutputStream out = null;
+			try {
+				out = ((HttpServletResponseImpl) response)
+						.getStaticFileOutputStream();
+				out.write(file);
+			} catch (IOException e) {
+				throw new HttpServerException(
+						"get static file output stream error");
+			} finally {
+				if (out != null)
+					try {
+						out.close();
+					} catch (IOException e) {
+						throw new HttpServerException(
+								"static file output stream close error");
+					}
+			}
+		} else {
+			SystemHtmlPage.responseSystemPage(request, response,
+					config.getEncoding(), HttpServletResponse.SC_NOT_FOUND,
+					request.getRequestURI() + " not found");
+		}
 	}
 
 }
