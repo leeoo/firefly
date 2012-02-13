e60606562dad6e2c200d866e61dad29637b4f373
baldulista@gmail.com <baldulista@gmail.com@4c6c975e-b6eb-aeeb-d668-919e69cb0141>
2012-02-13 08:46:53 +0000

    增加文件过滤器

--- trunk/firefly-core/src/main/java/com/firefly/server/http/FileDispatcherController.java
+++ trunk/firefly-core/src/main/java/com/firefly/server/http/FileDispatcherController.java
@@ -14,6 +14,7 @@ import com.firefly.server.exception.HttpServerException;
 import com.firefly.server.io.StaticFileOutputStream;
 import com.firefly.utils.RandomUtils;
 import com.firefly.utils.StringUtils;
+import com.firefly.utils.VerifyUtils;
 import com.firefly.utils.log.Log;
 import com.firefly.utils.log.LogFactory;
 
@@ -35,94 +36,92 @@ public class FileDispatcherController implements DispatcherController {
 	@Override
 	public void dispatcher(HttpServletRequest request,
 			HttpServletResponse response) {
-		File file = new File(config.getServerHome(), request.getRequestURI());
-		if (file.exists()) {
-			String fileSuffix = getFileSuffix(file.getName()).toLowerCase();
-			String contentType = Constants.MIME.get(fileSuffix);
-			if (contentType == null) {
-				response.setContentType("application/octet-stream");
+		String path = config.getFileAccessFilter().doFilter(request, response);
+		if (VerifyUtils.isEmpty(path)) {
+			SystemHtmlPage.responseSystemPage(request, response,
+					config.getEncoding(), HttpServletResponse.SC_NOT_FOUND,
+					request.getRequestURI() + " not found");
+			return;
+		}
+
+		File file = new File(config.getServerHome(), path);
+		if (!file.exists()) {
+			SystemHtmlPage.responseSystemPage(request, response,
+					config.getEncoding(), HttpServletResponse.SC_NOT_FOUND,
+					request.getRequestURI() + " not found");
+			return;
+		}
+
+		String fileSuffix = getFileSuffix(file.getName()).toLowerCase();
+		String contentType = Constants.MIME.get(fileSuffix);
+		if (contentType == null) {
+			response.setContentType("application/octet-stream");
+			response.setHeader("Content-Disposition", "attachment; filename="
+					+ file.getName());
+		} else {
+			String[] type = StringUtils.split(contentType, '/');
+			if ("application".equals(type[0])) {
 				response.setHeader("Content-Disposition",
 						"attachment; filename=" + file.getName());
-			} else {
-				String[] type = StringUtils.split(contentType, '/');
-				if ("application".equals(type[0])) {
-					response.setHeader("Content-Disposition",
-							"attachment; filename=" + file.getName());
-				} else if ("text".equals(type[0])) {
-					contentType += "; charset=" + config.getEncoding();
-				}
-				response.setContentType(contentType);
+			} else if ("text".equals(type[0])) {
+				contentType += "; charset=" + config.getEncoding();
 			}
+			response.setContentType(contentType);
+		}
 
-			StaticFileOutputStream out = null;
-			try {
-				out = ((HttpServletResponseImpl) response)
-						.getStaticFileOutputStream();
-				long fileLen = file.length();
-				String range = request.getHeader("Range");
-				if (range == null) {
-					out.write(file);
-				} else {
-					String[] rangesSpecifier = StringUtils.split(range, '=');
-					if (rangesSpecifier.length != 2) {
+		StaticFileOutputStream out = null;
+		try {
+			out = ((HttpServletResponseImpl) response)
+					.getStaticFileOutputStream();
+			long fileLen = file.length();
+			String range = request.getHeader("Range");
+			if (range == null) {
+				out.write(file);
+			} else {
+				String[] rangesSpecifier = StringUtils.split(range, '=');
+				if (rangesSpecifier.length != 2) {
+					response.setStatus(416);
+					out.write(RANGE_ERROR_HTML.getBytes(config.getEncoding()));
+					return;
+				}
+
+				String byteRangeSet = rangesSpecifier[1].trim();
+				String[] byteRangeSets = StringUtils.split(byteRangeSet, ',');
+				if (byteRangeSets.length > 1) { // multipart/byteranges
+					String boundary = "ff10" + getRandomString(13);
+					if (byteRangeSets.length > config.getMaxRangeNum()) {
+						log.error("multipart range more than {}",
+								config.getMaxRangeNum());
 						response.setStatus(416);
 						out.write(RANGE_ERROR_HTML.getBytes(config
 								.getEncoding()));
 						return;
 					}
-
-					String byteRangeSet = rangesSpecifier[1].trim();
-					String[] byteRangeSets = StringUtils.split(byteRangeSet,
-							',');
-					if (byteRangeSets.length > 1) { // multipart/byteranges
-						String boundary = "ff10" + getRandomString(13);
-						if (byteRangeSets.length > config.getMaxRangeNum()) {
-							log.error("multipart range more than {}",
-									config.getMaxRangeNum());
-							response.setStatus(416);
-							out.write(RANGE_ERROR_HTML.getBytes(config
-									.getEncoding()));
-							return;
-						}
-						// multipart output
-						List<MultipartByteranges> tmpByteRangeSets = new ArrayList<MultipartByteranges>(
-								config.getMaxRangeNum());
-						// long otherLen = 0;
-						for (String t : byteRangeSets) {
-							String tmp = t.trim();
-							String[] byteRange = StringUtils.split(tmp, '-');
-							if (byteRange.length == 1) {
-								long pos = Long.parseLong(byteRange[0].trim());
-								if (pos == 0)
+					// multipart output
+					List<MultipartByteranges> tmpByteRangeSets = new ArrayList<MultipartByteranges>(
+							config.getMaxRangeNum());
+					// long otherLen = 0;
+					for (String t : byteRangeSets) {
+						String tmp = t.trim();
+						String[] byteRange = StringUtils.split(tmp, '-');
+						if (byteRange.length == 1) {
+							long pos = Long.parseLong(byteRange[0].trim());
+							if (pos == 0)
+								continue;
+							if (tmp.charAt(0) == '-') {
+								long lastBytePos = fileLen - 1;
+								long firstBytePos = lastBytePos - pos + 1;
+								if (firstBytePos > lastBytePos)
 									continue;
-								if (tmp.charAt(0) == '-') {
-									long lastBytePos = fileLen - 1;
-									long firstBytePos = lastBytePos - pos + 1;
-									if (firstBytePos > lastBytePos)
-										continue;
 
-									MultipartByteranges multipartByteranges = getMultipartByteranges(
-											contentType, firstBytePos,
-											lastBytePos, fileLen, boundary);
-									tmpByteRangeSets.add(multipartByteranges);
-								} else if (tmp.charAt(tmp.length() - 1) == '-') {
-									long firstBytePos = pos;
-									long lastBytePos = fileLen - 1;
-									if (firstBytePos > lastBytePos)
-										continue;
-
-									MultipartByteranges multipartByteranges = getMultipartByteranges(
-											contentType, firstBytePos,
-											lastBytePos, fileLen, boundary);
-									tmpByteRangeSets.add(multipartByteranges);
-								}
-							} else {
-								long firstBytePos = Long.parseLong(byteRange[0]
-										.trim());
-								long lastBytePos = Long.parseLong(byteRange[1]
-										.trim());
-								if (firstBytePos > fileLen
-										|| firstBytePos >= lastBytePos)
+								MultipartByteranges multipartByteranges = getMultipartByteranges(
+										contentType, firstBytePos, lastBytePos,
+										fileLen, boundary);
+								tmpByteRangeSets.add(multipartByteranges);
+							} else if (tmp.charAt(tmp.length() - 1) == '-') {
+								long firstBytePos = pos;
+								long lastBytePos = fileLen - 1;
+								if (firstBytePos > lastBytePos)
 									continue;
 
 								MultipartByteranges multipartByteranges = getMultipartByteranges(
@@ -130,25 +129,63 @@ public class FileDispatcherController implements DispatcherController {
 										fileLen, boundary);
 								tmpByteRangeSets.add(multipartByteranges);
 							}
+						} else {
+							long firstBytePos = Long.parseLong(byteRange[0]
+									.trim());
+							long lastBytePos = Long.parseLong(byteRange[1]
+									.trim());
+							if (firstBytePos > fileLen
+									|| firstBytePos >= lastBytePos)
+								continue;
+
+							MultipartByteranges multipartByteranges = getMultipartByteranges(
+									contentType, firstBytePos, lastBytePos,
+									fileLen, boundary);
+							tmpByteRangeSets.add(multipartByteranges);
+						}
+					}
+
+					if (tmpByteRangeSets.size() > 0) {
+						response.setStatus(206);
+						response.setHeader("Accept-Ranges", "bytes");
+						response.setHeader("Content-Type",
+								"multipart/byteranges; boundary=" + boundary);
+
+						for (MultipartByteranges m : tmpByteRangeSets) {
+							long length = m.lastBytePos - m.firstBytePos + 1;
+							out.write(m.head.getBytes(config.getEncoding()));
+							out.write(file, m.firstBytePos, length);
 						}
 
-						if (tmpByteRangeSets.size() > 0) {
-							response.setStatus(206);
-							response.setHeader("Accept-Ranges", "bytes");
-							response.setHeader("Content-Type",
-									"multipart/byteranges; boundary="
-											+ boundary);
+						out.write((CRLF + "--" + boundary + "--" + CRLF)
+								.getBytes(config.getEncoding()));
+						log.debug("multipart download|{}", range);
+					} else {
+						response.setStatus(416);
+						out.write(RANGE_ERROR_HTML.getBytes(config
+								.getEncoding()));
+						return;
+					}
+				} else {
+					String tmp = byteRangeSets[0].trim();
+					String[] byteRange = StringUtils.split(tmp, '-');
+					if (byteRange.length == 1) {
+						long pos = Long.parseLong(byteRange[0].trim());
+						if (pos == 0) {
+							response.setStatus(416);
+							out.write(RANGE_ERROR_HTML.getBytes(config
+									.getEncoding()));
+							return;
+						}
 
-							for (MultipartByteranges m : tmpByteRangeSets) {
-								long length = m.lastBytePos - m.firstBytePos
-										+ 1;
-								out.write(m.head.getBytes(config.getEncoding()));
-								out.write(file, m.firstBytePos, length);
-							}
-
-							out.write((CRLF + "--" + boundary + "--" + CRLF)
-									.getBytes(config.getEncoding()));
-							log.debug("multipart download|{}", range);
+						if (tmp.charAt(0) == '-') {
+							long lastBytePos = fileLen - 1;
+							long firstBytePos = lastBytePos - pos + 1;
+							writePartialFile(request, response, out, file,
+									firstBytePos, lastBytePos, fileLen);
+						} else if (tmp.charAt(tmp.length() - 1) == '-') {
+							writePartialFile(request, response, out, file, pos,
+									fileLen - 1, fileLen);
 						} else {
 							response.setStatus(416);
 							out.write(RANGE_ERROR_HTML.getBytes(config
@@ -156,70 +193,36 @@ public class FileDispatcherController implements DispatcherController {
 							return;
 						}
 					} else {
-						String tmp = byteRangeSets[0].trim();
-						String[] byteRange = StringUtils.split(tmp, '-');
-						if (byteRange.length == 1) {
-							long pos = Long.parseLong(byteRange[0].trim());
-							if (pos == 0) {
-								response.setStatus(416);
-								out.write(RANGE_ERROR_HTML.getBytes(config
-										.getEncoding()));
-								return;
-							}
-
-							if (tmp.charAt(0) == '-') {
-								long lastBytePos = fileLen - 1;
-								long firstBytePos = lastBytePos - pos + 1;
-								writePartialFile(request, response, out, file,
-										firstBytePos, lastBytePos, fileLen);
-							} else if (tmp.charAt(tmp.length() - 1) == '-') {
-								writePartialFile(request, response, out, file,
-										pos, fileLen - 1, fileLen);
-							} else {
-								response.setStatus(416);
-								out.write(RANGE_ERROR_HTML.getBytes(config
-										.getEncoding()));
-								return;
-							}
-						} else {
-							long firstBytePos = Long.parseLong(byteRange[0]
-									.trim());
-							long lastBytePos = Long.parseLong(byteRange[1]
-									.trim());
-							if (firstBytePos > fileLen
-									|| firstBytePos >= lastBytePos) {
-								response.setStatus(416);
-								out.write(RANGE_ERROR_HTML.getBytes(config
-										.getEncoding()));
-								return;
-							}
-
-							if (lastBytePos >= fileLen)
-								lastBytePos = fileLen - 1;
-
-							writePartialFile(request, response, out, file,
-									firstBytePos, lastBytePos, fileLen);
+						long firstBytePos = Long.parseLong(byteRange[0].trim());
+						long lastBytePos = Long.parseLong(byteRange[1].trim());
+						if (firstBytePos > fileLen
+								|| firstBytePos >= lastBytePos) {
+							response.setStatus(416);
+							out.write(RANGE_ERROR_HTML.getBytes(config
+									.getEncoding()));
+							return;
 						}
-						log.debug("single range download|{}", range);
+
+						if (lastBytePos >= fileLen)
+							lastBytePos = fileLen - 1;
+
+						writePartialFile(request, response, out, file,
+								firstBytePos, lastBytePos, fileLen);
 					}
+					log.debug("single range download|{}", range);
 				}
-			} catch (Throwable e) {
-				throw new HttpServerException(
-						"get static file output stream error");
-			} finally {
-				if (out != null)
-					try {
-						// System.out.println("close~~");
-						out.close();
-					} catch (IOException e) {
-						throw new HttpServerException(
-								"static file output stream close error");
-					}
 			}
-		} else {
-			SystemHtmlPage.responseSystemPage(request, response,
-					config.getEncoding(), HttpServletResponse.SC_NOT_FOUND,
-					request.getRequestURI() + " not found");
+		} catch (Throwable e) {
+			throw new HttpServerException("get static file output stream error");
+		} finally {
+			if (out != null)
+				try {
+					// System.out.println("close~~");
+					out.close();
+				} catch (IOException e) {
+					throw new HttpServerException(
+							"static file output stream close error");
+				}
 		}
 	}
 
